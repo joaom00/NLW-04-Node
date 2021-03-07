ed0250d4cccb4940f408876992c19884f5678528
joaom <joaomljcanal@gmail.com>
2021-03-07 20:20:09 -0300

    Add survey route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import SurveyController from './controllers/SurveyController';
 import UserController from './controllers/UserController';
 
 const router = Router();
 
 const usersController = new UserController();
+const surveysController = new SurveyController();
 
 router.post('/users', usersController.create);
+router.post('/surveys', surveysController.create);
 
 export default router;
