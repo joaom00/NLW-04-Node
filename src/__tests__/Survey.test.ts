dc3e96cc1962f39272d0f076e6123a5efbbe0594
joaom <joaomljcanal@gmail.com>
2021-03-07 21:16:56 -0300

    test: should be able to get all surveys

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -18,4 +18,15 @@ describe('Surveys', () => {
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('id');
   });
+
+  it('Should be able to get all surveys', async () => {
+    await request(app).post('/surveys').send({
+      title: 'Title Example2',
+      description: 'Description Example2',
+    });
+
+    const response = await request(app).get('/surveys');
+
+    expect(response.body.length).toBe(2);
+  });
 });
