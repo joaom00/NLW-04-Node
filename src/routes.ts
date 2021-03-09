074a4922e7342ee8657821c2cf7682126823fe82
joaom <joaomljcanal@gmail.com>
2021-03-09 13:19:28 -0300

    answer route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import AnswerController from './controllers/AnswerController';
 import SendEmailController from './controllers/SendEmailController';
 import SurveyController from './controllers/SurveyController';
 import UserController from './controllers/UserController';
@@ -8,6 +9,7 @@ const router = Router();
 const usersController = new UserController();
 const surveysController = new SurveyController();
 const sendMailController = new SendEmailController();
+const answerController = new AnswerController();
 
 router.post('/users', usersController.create);
 
@@ -15,5 +17,7 @@ router.get('/surveys', surveysController.show);
 router.post('/surveys', surveysController.create);
 
 router.post('/sendMail', sendMailController.execute);
+
+router.get('/answer', answerController.execute);
 
 export default router;
